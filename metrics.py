579b82b290f1797837a0f1da0e1df8ef8a227948
LiuLei <1119016521@qq.com>
2022-04-14 13:26:34 +0800

    add another check

--- metrics.py
+++ metrics.py
@@ -42,26 +42,53 @@ def detectCpuAndMemById(exp_config, pod_id):
             writer.writerow([str(now_date), cpu, mem])
         time.sleep(15)
 
+# if __name__ == '__main__':
+#
+#     exp_config = {
+#         "experiment":{
+#             "namespace": "default",
+#             "metrics-data": "./"
+#         }
+#     }
+#     podList= ["notificationclient-service-78bd8b4689-mhmnt", "notificationclient-service-78bd8b4689-zgpcz"]
+#     pool = multiprocessing.Pool(processes=len(podList))
+#     for i in podList:
+#         res = pool.apply_async(detectCpuAndMemById, args=[exp_config, i])
+#     pool.close()
+#
+#     #主线程
+#     print("主线程")
+#     time.sleep(20)
+#
+#     # 关掉线程
+#     pool.terminate()
+
+
+
+
+def detectCpuAndMemByPodId( pod_id):
+    shell = 'kubectl describe pods  ' + str(pod_id)   + " | grep Container | grep docker"
+    res = os.popen(shell).readlines()
+    h = re.sub(' +', " ", res[0].strip('\n')).strip()
+    contrainerid = h.split("//")[1]
+    filename = "./" + str(pod_id) + ".csv"
+    with open(filename, "a+", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(["date", "cpuUseage", "mem", 'memlimit'])
+    while True:
+        res = os.popen("docker stats " + str(contrainerid) + " --no-stream").readlines()
+        h = re.sub(' +', " ", res[1].strip('\n'))
+        result = h.split(" ")
+        print(result)
+        cpuUseage = result[2]
+        mem = result[3]
+        memlimit = result[5]
+        now_date = datetime.datetime.now()
+        with open(filename, "a+", newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow([str(now_date), cpuUseage, mem, memlimit])
+        time.sleep(1)
+
+
 if __name__ == '__main__':
-
-    exp_config = {
-        "experiment":{
-            "namespace": "default",
-            "metrics-data": "./"
-        }
-    }
-    podList= ["notificationclient-service-78bd8b4689-mhmnt", "notificationclient-service-78bd8b4689-zgpcz"]
-    pool = multiprocessing.Pool(processes=len(podList))
-    for i in podList:
-        res = pool.apply_async(detectCpuAndMemById, args=[exp_config, i])
-    pool.close()
-
-    #主线程
-    print("主线程")
-    time.sleep(20)
-
-    # 关掉线程
-    pool.terminate()
-
-
-
+    detectCpuAndMemByPodId("basicuser-service-68f84d498f-qrl92")
